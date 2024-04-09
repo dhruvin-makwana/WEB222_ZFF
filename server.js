68f2673121fcc6d0797d17abb054671168b32c37
dhruvin-makwana <dhruvin.makwana@senecapolytechnic.ca>
2024-04-09 00:57:46 -0400

    Added bitcoin watcher example

---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+app.use(cors());
+const port = 8001;
+
+app.get('/api', (req, res) => {
+  let price = 67697.64;
+  const perc = Math.random() * 2 - 1;
+  price = price + (price * perc/100);
+  price=Math.round(price*100)/100
+  res.send(price.toString());
+});
+
+app.get('/', (req, res) => {
+  let price = 67697.64;
+  const perc = Math.random() * 2 - 1;
+  price = price + (price * perc/100);
+  price=Math.round(price*100)/100
+  res.send(`Bitcoin Price: ${price} (USD)`);
+});
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
